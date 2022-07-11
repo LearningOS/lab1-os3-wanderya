cbe6f1b29100ca917048e03ae38bfe23e11901df
wangtao <2942960798@qq.com>
2022-07-11 21:33:51 +0800

    modified ci-user/overwirte.py

--- ci-user/overwrite.py
+++ ci-user/overwrite.py
@@ -22,7 +22,7 @@ elif chapter <= 8:
 lines = []
 with open("../os/Cargo.toml", 'r') as f:
     for line in f.readlines():
-        processed = line.replace(' git = "https://github.com/rcore-os/riscv"', ' path = "../ci-user/riscv" ')
-        lines.append(processed)
+        # processed = line.replace(' git = "https://gitee.com/rcore-os/riscv"', ' path = "../ci-user/riscv" ')
+        lines.append(line)
 with open("../os/Cargo.toml", 'w+') as f:
     f.writelines(lines)
